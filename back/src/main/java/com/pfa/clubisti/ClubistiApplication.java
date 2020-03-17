d421938f60166b21a5f6052fd238259642266c81
rached1997 <hawejdene@gmail.com>
2020-03-17 16:16:13 +0100

    first commit

---
+++ back/src/main/java/com/pfa/clubisti/ClubistiApplication.java
@@ -0,0 +1,13 @@
+package com.pfa.clubisti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubistiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubistiApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/pfa/clubisti/ClubistiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfa.clubisti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubistiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
