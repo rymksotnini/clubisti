eb1e610654db6fad91f0f3490e6cb5e2bbf133ce
rached1997 <hawejdene@gmail.com>
2020-03-23 10:15:05 +0100

    add configuration

---
+++ back/src/main/java/com/pfa/clubisti/config/WebSecurityConfiguration.java
@@ -0,0 +1,15 @@
+package com.pfa.clubisti.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+    }
+}
