f880d949c75632695182fafe4c1e29457eeaa801
rymksontini <rymksontini@gmail.com>
2020-03-23 19:11:15 +0100

    securityConfig file

--- back/src/main/java/com/pfa/clubisti/security/SecurityConfig.java
+++ back/src/main/java/com/pfa/clubisti/security/SecurityConfig.java
@@ -11,6 +11,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http.cors()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/register").permitAll()
+                .antMatchers(HttpMethod.GET, "/user/info", "/api/foos/**")
+                .hasAuthority("SCOPE_read")
+                .antMatchers(HttpMethod.POST, "/api/foos")
+                .hasAuthority("SCOPE_write")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
     }
 }
