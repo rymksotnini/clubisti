d6ed52b63e83c231f5da863e49c3978aeb691607
rached1997 <hawejdene@gmail.com>
2020-06-06 20:41:23 +0100

    add mail service

---
+++ backPHP/app/Mail/MailService.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Mail;
+use Mail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class MailService extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $details;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Mail from clubisti')
+            ->view('emails.verifyEmail');
+    }
+
+     public function sendTo($to_name, $to_email, $data, $template, $subject, $title) {
+
+            Mail::send($template, $data, function($message) use ($to_name, $to_email, $subject, $title) {
+
+            $message->to($to_email, $to_name)
+            ->subject($subject);
+            $message->from('haouari.wejdene2@gmail.com',$title);
+            });
+
+            }
+
+    }
+
+
+
