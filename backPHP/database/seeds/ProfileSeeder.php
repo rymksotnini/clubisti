71d3293e98c0444d860fc8bc514b8b581b053839
rymksontini <rymksontini@gmail.com>
2020-06-09 14:48:24 +0100

    database seeding and long description correction

---
+++ backPHP/database/seeds/ProfileSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProfileSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('profiles')->insert([
+            'user_id' => 1,
+            'badge_id' => 1
+        ]);
+    }
+}
